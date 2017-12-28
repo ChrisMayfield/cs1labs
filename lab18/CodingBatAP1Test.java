d814a8d1aab127762ec5714bdc71c5ab920cda9a
Chris Mayfield <mayfiecs@jmu.edu>
2017-12-28 17:02:43 -0500

    revised Lab17 and Lab18

--- lab18/CodingBatAP1Test.java
+++ lab18/CodingBatAP1Test.java
@@ -1,4 +1,5 @@
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 /**
  * Test methods for CodingBat AP-1.
@@ -6,28 +7,34 @@ import junit.framework.TestCase;
  * @author YOUR NAME
  * @version DUE DATE
  */
-public class CodingBatAP1Test extends TestCase {
+public class CodingBatAP1Test {
     
+    @Test
     public void testScoresIncreasing() {
         fail("Not yet implemented");
     }
     
+    @Test
     public void testScores100() {
         fail("Not yet implemented");
     }
     
+    @Test
     public void testScoresClump() {
         fail("Not yet implemented");
     }
     
+    @Test
     public void testScoresAverage() {
         fail("Not yet implemented");
     }
     
+    @Test
     public void testWordsCount() {
         fail("Not yet implemented");
     }
     
+    @Test
     public void testWordsFront() {
         fail("Not yet implemented");
     }
